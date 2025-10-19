0c81923de652beaad1e4e2dbf41d3639a922dd3b
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-10-19 16:03:50 +0200

    Fix/app closing and cmd list allocators usage (#8)

--- RayTracing/RayTracing/Source/RenderAPI/D3D12/Command/D3D12CommandList.cpp
+++ RayTracing/RayTracing/Source/RenderAPI/D3D12/Command/D3D12CommandList.cpp
@@ -4,22 +4,26 @@
 
 void D3D12CommandList::Initialize(ID3D12Device* pDevice)
 {
-	HRESULT hr = pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(mCommandAllocator.ReleaseAndGetAddressOf()));
-	ASSERT_HR(hr, "Failed to create command allocator.");
+	HRESULT hr;
+	for (auto& allocator : mCommandAllocators)
+	{
+		hr = pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(allocator.ReleaseAndGetAddressOf()));
+		ASSERT_HR(hr, "Failed to create command allocator.");
+	}
 
-	hr = pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, mCommandAllocator.Get(), nullptr, IID_PPV_ARGS(mCommandList.ReleaseAndGetAddressOf()));
+	hr = pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, mCommandAllocators[0].Get(), nullptr, IID_PPV_ARGS(mCommandList.ReleaseAndGetAddressOf()));
 	ASSERT_HR(hr, "Failed to create command list.");
 
 	hr = mCommandList->Close();
 	ASSERT_HR(hr, "Failed to close command list after creation.");
 }
 
-void D3D12CommandList::ResetCommandList()
+void D3D12CommandList::ResetCommandList(UINT frameIndex)
 {
-	HRESULT hr = mCommandAllocator->Reset();
+	HRESULT hr = mCommandAllocators[frameIndex]->Reset();
 	ASSERT_HR(hr, "Failed to reset command allocator.");
 
-	hr = mCommandList->Reset(mCommandAllocator.Get(), nullptr);
+	hr = mCommandList->Reset(mCommandAllocators[frameIndex].Get(), nullptr);
 	ASSERT_HR(hr, "Failed to reset command list.");
 }
 
