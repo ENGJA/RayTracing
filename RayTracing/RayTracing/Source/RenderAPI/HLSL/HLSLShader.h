acad954312584f8de40da385c21671fa9290d82f
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-10-20 19:59:46 +0200

    Set basic pipeline

--- RayTracing/RayTracing/Source/RenderAPI/HLSL/HLSLShader.h
+++ RayTracing/RayTracing/Source/RenderAPI/HLSL/HLSLShader.h
@@ -4,6 +4,7 @@ class HLSLShader
 private:
 	Microsoft::WRL::ComPtr<IDxcBlob> mShaderBlob;
 public:
+	HLSLShader() = default;
 	HLSLShader(IDxcBlob* shaderBlob) : mShaderBlob(shaderBlob) {}
 	IDxcBlob* GetShaderBlob() const { return mShaderBlob.Get(); }
 };
--- RayTracing/RayTracing/Source/RenderAPI/Renderer.cpp
+++ RayTracing/RayTracing/Source/RenderAPI/Renderer.cpp
@@ -62,10 +62,27 @@ void Renderer::Initialize(HWND hwnd, UINT width, UINT height)
 
 	HLSLCompiler compiler;
 	compiler.Initialize();
-	HLSLShader testShader = compiler.CompileFromFile(L"Source/Shaders/VertexShader.hlsl", L"vs_6_0");
-	// temporary end
 
-	mRootSignature.Initialize(mDevice.Get());
+	//HLSLShader vertexShader = compiler.LoadFromCso(L"VertexShader.cso"); // has to be run from .exe path
+	HLSLShader vertexShader = compiler.CompileFromFile(L"Source/Shaders/VertexShader.hlsl", L"vs_6_0");
+	HLSLShader pixelShader = compiler.CompileFromFile(L"Source/Shaders/PixelShader.hlsl", L"ps_6_0");
+
+
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D12_APPEND_ALIGNED_ELEMENT, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
+	D3D12_INPUT_LAYOUT_DESC inputLayoutDesc{};
+	inputLayoutDesc.pInputElementDescs = inputElementDescs;
+	inputLayoutDesc.NumElements = _countof(inputElementDescs);
+
+
+	mPipelineState.Initialize(
+		mDevice.Get(),
+		std::move(vertexShader),
+		std::move(pixelShader),
+		inputLayoutDesc);
 }
 
 
