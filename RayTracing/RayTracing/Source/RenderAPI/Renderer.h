b8db97b9d531b6f7733092888dcb052f81b677d3
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-11-26 23:41:28 +0100

    Update brief

--- RayTracing/RayTracing/Source/RenderAPI/Renderer.h
+++ RayTracing/RayTracing/Source/RenderAPI/Renderer.h
@@ -117,6 +117,8 @@ public:
 	/**
 	 * @brief Records and submits commands for one frame and presents.
 	 * @param viewProj View-projection matrix dostarczony z zewn¹trz (CameraManager).
+	 * @param cameraPos Camera world position.
+	 * @param cameraForward Camera forward vector.
 	 */
 	void Update(const DirectX::XMMATRIX& viewProj, const DirectX::XMFLOAT3& cameraPos, const DirectX::XMFLOAT3& cameraForward);
 };
