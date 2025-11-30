8958170d6640812915ecf02555d53c728db3ff44
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-11-30 23:45:02 +0100

    Export common code in D3D12PipelineState

--- RayTracing/RayTracing/Source/RenderAPI/D3D12/D3D12PipelineState.cpp
+++ RayTracing/RayTracing/Source/RenderAPI/D3D12/D3D12PipelineState.cpp
@@ -6,30 +6,13 @@
 
 void D3D12PipelineState::InitializeOpaque(ID3D12Device* pDevice, HLSLShader vertexShader, HLSLShader pixelShader, const D3D12_INPUT_LAYOUT_DESC& inputLayoutDesc)
 {
-	mRootSignature.Initialize(pDevice);
-	mVertexShader = std::move(vertexShader);
-	mPixelShader = std::move(pixelShader);
+	InitializeCommon(pDevice, std::move(vertexShader), std::move(pixelShader));
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpsDesc = MakeBaseDesc(inputLayoutDesc);
 
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpsDesc{};
-	gpsDesc.pRootSignature = mRootSignature.Get();
-	gpsDesc.VS.pShaderBytecode = mVertexShader.GetShaderBlob()->GetBufferPointer();
-	gpsDesc.VS.BytecodeLength = mVertexShader.GetShaderBlob()->GetBufferSize();
-	gpsDesc.PS.pShaderBytecode = mPixelShader.GetShaderBlob()->GetBufferPointer();
-	gpsDesc.PS.BytecodeLength = mPixelShader.GetShaderBlob()->GetBufferSize();
-	gpsDesc.DepthStencilState.DepthEnable = TRUE;
+	// Specific changes for opaque
 	gpsDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
-	gpsDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
-	gpsDesc.DSVFormat = Config::cDepthBufferFormat;
-	gpsDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-	gpsDesc.NumRenderTargets = 1;
-	gpsDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-	gpsDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
-	gpsDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
-	gpsDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-	gpsDesc.SampleMask = UINT_MAX;
-	gpsDesc.SampleDesc = { 1, 0 };
-	gpsDesc.InputLayout = inputLayoutDesc;
-
+	gpsDesc.BlendState.RenderTarget[0].BlendEnable = FALSE;
+	//gpsDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
 
 	HRESULT hr = pDevice->CreateGraphicsPipelineState(
 		&gpsDesc,
@@ -40,25 +23,11 @@ void D3D12PipelineState::InitializeOpaque(ID3D12Device* pDevice, HLSLShader vert
 
 void D3D12PipelineState::InitializeTransparent(ID3D12Device* pDevice, HLSLShader vertexShader, HLSLShader pixelShader, const D3D12_INPUT_LAYOUT_DESC& inputLayoutDesc)
 {
-	mRootSignature.Initialize(pDevice);
-	mVertexShader = std::move(vertexShader);
-	mPixelShader = std::move(pixelShader);
+	InitializeCommon(pDevice, std::move(vertexShader), std::move(pixelShader));
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpsDesc = MakeBaseDesc(inputLayoutDesc);
 
-	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpsDesc{};
-	gpsDesc.pRootSignature = mRootSignature.Get();
-	gpsDesc.VS.pShaderBytecode = mVertexShader.GetShaderBlob()->GetBufferPointer();
-	gpsDesc.VS.BytecodeLength = mVertexShader.GetShaderBlob()->GetBufferSize();
-	gpsDesc.PS.pShaderBytecode = mPixelShader.GetShaderBlob()->GetBufferPointer();
-	gpsDesc.PS.BytecodeLength = mPixelShader.GetShaderBlob()->GetBufferSize();
-	gpsDesc.DepthStencilState.DepthEnable = TRUE;
+	// Specific changes for transparent
 	gpsDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
-	gpsDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
-	gpsDesc.DSVFormat = Config::cDepthBufferFormat;
-	gpsDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-	gpsDesc.NumRenderTargets = 1;
-	gpsDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-	gpsDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
-	gpsDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
 	gpsDesc.BlendState.RenderTarget[0].BlendEnable = TRUE;
 	gpsDesc.BlendState.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
 	gpsDesc.BlendState.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
@@ -66,15 +35,56 @@ void D3D12PipelineState::InitializeTransparent(ID3D12Device* pDevice, HLSLShader
 	gpsDesc.BlendState.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
 	gpsDesc.BlendState.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
 	gpsDesc.BlendState.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
-	gpsDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-	gpsDesc.SampleMask = UINT_MAX;
-	gpsDesc.SampleDesc = { 1, 0 };
-	gpsDesc.InputLayout = inputLayoutDesc;
-
+	//gpsDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
 
 	HRESULT hr = pDevice->CreateGraphicsPipelineState(
 		&gpsDesc,
 		IID_PPV_ARGS(mPipelineState.ReleaseAndGetAddressOf())
 	);
 	ASSERT_HR(hr, "Failed to create pipeline state object.");
+}
+
+D3D12_GRAPHICS_PIPELINE_STATE_DESC D3D12PipelineState::MakeBaseDesc(const D3D12_INPUT_LAYOUT_DESC& inputLayoutDesc) const
+{
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC gpsDesc{};
+	gpsDesc.pRootSignature = mRootSignature.Get();
+
+	// Shaders
+	gpsDesc.VS.pShaderBytecode = mVertexShader.GetShaderBlob()->GetBufferPointer();
+	gpsDesc.VS.BytecodeLength = mVertexShader.GetShaderBlob()->GetBufferSize();
+	gpsDesc.PS.pShaderBytecode = mPixelShader.GetShaderBlob()->GetBufferPointer();
+	gpsDesc.PS.BytecodeLength = mPixelShader.GetShaderBlob()->GetBufferSize();
+
+	// Depth-stencil
+	gpsDesc.DepthStencilState.DepthEnable = TRUE;
+	gpsDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+	gpsDesc.DSVFormat = Config::cDepthBufferFormat;
+
+	// Topology and render target formats
+	gpsDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	gpsDesc.NumRenderTargets = 1;
+	gpsDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	// Rasterizer
+	gpsDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+	gpsDesc.RasterizerState.CullMode = D3D12_CULL_MODE_BACK;
+
+	// Blend
+	gpsDesc.BlendState.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+
+	// Sampling
+	gpsDesc.SampleMask = UINT_MAX;
+	gpsDesc.SampleDesc = { 1, 0 };
+
+	// Input layout
+	gpsDesc.InputLayout = inputLayoutDesc;
+
+	return gpsDesc;
+}
+
+void D3D12PipelineState::InitializeCommon(ID3D12Device* pDevice, HLSLShader vertexShader, HLSLShader pixelShader)
+{
+	mRootSignature.Initialize(pDevice);
+	mVertexShader = std::move(vertexShader);
+	mPixelShader = std::move(pixelShader);
 }
