9df5e2e29abf9cd87ffe2309892bdc689e17dea0
Finger2003 <131339144+Finger2003@users.noreply.github.com>
2025-12-03 21:12:41 +0100

    Add BLAS and TLAS building

--- RayTracing/RayTracing/Source/RenderAPI/Renderer.h
+++ RayTracing/RayTracing/Source/RenderAPI/Renderer.h
@@ -14,6 +14,7 @@
 #include "ResourceLoading/TextureLoader.h"
 #include "RenderAPI/Descriptors/ShaderVisibleDescriptorHeap.h"
 #include "RenderAPI/HLSL/HLSLCompiler.h"
+#include "RenderAPI/RT/RayTracingBuilder.h"
 #include <unordered_map>
 
 /**
@@ -41,6 +42,9 @@ struct MeshGpuData
 
 	DirectX::XMFLOAT3 center; ///< Mesh bounding sphere center in model space.
 	float distanceToCamera = 0.0f; ///< Distance from mesh center to camera (for sorting).
+
+	D3D12Resource blasResult; ///< Bottom-level acceleration structure resource for ray tracing.
+	//UINT blasIndex;
 };
 
 struct DefaultTextures
@@ -94,6 +98,12 @@ private:
 	std::vector<MeshGpuData> mMaskedDoubleSidedMeshes; ///< Flattened array of masked double-sided mesh GPU data for rendering.
 
 	std::vector<LightData> mStaticLights; ///< Static lights loaded from models.
+
+	RayTracingBuilder mRayTracingBuilder; ///< Ray tracing acceleration structure builder.
+	D3D12Resource mTLAS;	///< Top-level acceleration structure result.
+	D3D12Resource mTLAS_Scratch;	///< Top-level acceleration structure scratch buffer. May be used during updating, when objects move.
+	D3D12Resource mInstanceDescBuffer;	///< Instance descriptions buffer for TLAS. TBH I don't know if it should be kept around after build.
+
 
 	HLSLCompiler mShaderCompiler; ///< HLSL shader compiler instance.
 
@@ -178,6 +188,11 @@ private:
 	 * @brief Initializes default dummy textures (white, normal).
 	 */
 	void InitializeDummyTextures();
+
+	/**
+	 * @brief Initializes ray tracing acceleration structures.
+	 */
+	void InitializeRayTracing();
 public:
 	/**
 	 * @brief Creates device/swap chain and initializes resources.
